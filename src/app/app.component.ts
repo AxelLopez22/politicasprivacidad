9b7d1d875f4224481558e835161fb40fe08c8f80
Laptop Hensell Espinoza <DevMovil@pcg.domain>
2023-12-27 15:08:17 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatToolbarModule, MatButtonModule, MatIconModule,
+    MatDividerModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
